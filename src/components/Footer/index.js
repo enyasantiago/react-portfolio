c172c8275b615f368256846c0631ddf20df91203
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-05-17 22:03:38 -0400

    added header with typewriter effect

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer className="py-4 bg-secondary">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright &copy; 2021
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+import Typewriter from "typewriter-effect";
+import PortfolioImages from "../../images/Enya.jpg";
+
+function Header() {
+  return (
+    <>
+    <main class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+            <img src ={PortfolioImages} alt= "Enya" className= "img-fluid"/>
+            </div>
+
+            <div class="col-12 col-sm-6" id="about">
+            <h1><Typewriter
+              options={{
+                strings: "Hi, I'm Enya.",
+                pauseFor: 10000,
+                autoStart: true,
+                loop: true,
+                }}
+                />
+                </h1>
+            
+            <h3>I'm a full stack developer-in-training, hospice nurse, massage therapist and lifelong learner.</h3>
+            </div>  
+        </div>
+    </main>  
+    </>
+  );
+}
+
+export default Header;
