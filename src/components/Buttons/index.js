83e16353d593daa7fa860f276af0f675a4adb731
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-05-18 01:38:57 -0400

    fixing buttons

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 
-export function Buttons(props) {
+export default function Buttons(props) {
   return (
     <div className="landing_btn_container">
       <Link className="btn btn-outline-secondary" role="button" {...props} />
     </div>
   );
 }
-// export default Buttons;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,35 +7,25 @@ import Budget from "../../images/Budget.png";
 import Burger from "../../images/Burgers.gif";
 import Buttons from "../Buttons";
 import Card from "../Card";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
-// import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 // import { faGitHub } from "@fortawesome/free-solid-svg-icons";
 
 function Portfolio(props) {
     return (
     <>
-        {/* <div class="card">
-            <img src ={PortfolioImages} alt= "Enya" className= "img-fluid"/>
-            <div class="card-body">
-                    <h5 class="card-title">Style Me</h5>
-                    <p class="card-text">Style Me is an application that allows the user to upload photos of your clothing from your camera,
-                        computer or social media.
-                        Organize your clothes into outfits and categorize them by occasion, season, mood or whatever you like!
-                        Create your own personal lookbook and maximize your closet.
-                    </p>
-                    <div class="btn-group" role="group" aria-label="projects">
-                        <a class="btn btn-secondary btn-lg" href="https://stylemenow.herokuapp.com/" role="button"><i class="fas fa-external-link-alt"></i></a>
-                        <a class="btn btn-secondary btn-lg" href="https://github.com/mpasricha1/style_me" role="button"><i class="fab fa-github-square"></i></a>
-                    </div>
-                </div>
-        </div>     */}
+        <div class="card">
         <Card
             PortfolioImages={StyleMe}
             imgAlt={"Style Me"}
             cardTitle={"Style Me"}
             cardText={"Style Me is an application that allows the user to upload photos of your clothing from your camera,computer or social media. Organize your clothes into outfits and categorize them by occasion, season, mood or whatever you like! Create your own personal lookbook and maximize your closet."}
         />
+        <div class="btn-group" role="group" aria-label="projects">
+            {/* <button><FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></button> */}
+            <Buttons to={{ pathname: "https://enyasantiago.github.io/" }} target="_blank"> app </Buttons>
+        </div>
+        </div>
         <Card
             PortfolioImages={MixTape}
             imgAlt={"Mix Tape"}
