8d4df4f7e6be7cd7cf28bfa6776d4c1d9f0e64cc
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-05-20 11:42:14 -0400

    commit to redeploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ContactPage from "./pages/contact";
 function App() {
   return (
     <div className="App">
-      <Router basename='/react-portfolio'>
+      <Router basename='/'>
       <Switch>
         <Route exact path={["/", "/about"]}>
           <About />
